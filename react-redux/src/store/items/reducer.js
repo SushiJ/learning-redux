a27f381eb6f5e42b65111aa1cb0c6e1e0810a5ff
Sushij <sushant.wurk@gmail.com>
2023-02-26 19:16:28 +0530

    Added Immer for state management Immutably

--- react-redux/src/store/items/reducer.js
+++ react-redux/src/store/items/reducer.js
@@ -1,10 +1,16 @@
-import { ITEM_ADDED, ITEM_PRICE_UPDATED, ITEM_QUANTITY_UPDATED, ITEM_REMOVED } from "./actions";
+import produce from "immer";
+import {
+  ITEM_ADDED,
+  ITEM_PRICE_UPDATED,
+  ITEM_QUANTITY_UPDATED,
+  ITEM_REMOVED,
+} from "./actions";
 
 let id = 1;
 
 export const initialItems = [
-  { uuid: id++, name: 'Awesome Tofu Roast', price: 14, quantity: 1 },
-  { uuid: id++, name: 'Vegan ham sandwich', price: 12, quantity: 1 }
+  { uuid: id++, name: "Awesome Tofu Roast", price: 14, quantity: 1 },
+  { uuid: id++, name: "Vegan ham sandwich", price: 12, quantity: 1 },
 ];
 
 export const reducer = (state = initialItems, action) => {
@@ -12,28 +18,26 @@ export const reducer = (state = initialItems, action) => {
     const item = {
       uuid: id++,
       quantity: 1,
-      ...action.payload
-    }
-    return [...state, item]
+      ...action.payload,
+    };
+    return produce(state, (draftState) => {
+      draftState.push(item);
+    });
   }
   if (action.type === ITEM_REMOVED) {
-    return state.filter(item => item.uuid !== action.payload.uuid)
+    return state.filter((item) => item.uuid !== action.payload.uuid);
   }
   if (action.type === ITEM_PRICE_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, price: action.payload.price }
-      }
-      return item
-    })
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.price = parseInt(action.payload.price, 10);
+    });
   }
   if (action.type === ITEM_QUANTITY_UPDATED) {
-    return state.map(item => {
-      if (item.uuid === action.payload.uuid) {
-        return { ...item, quantity: action.payload.quantity }
-      }
-      return item
-    })
+    return produce(state, (draftState) => {
+      const item = draftState.find((item) => item.uuid === action.payload.uuid);
+      item.quantity = parseInt(action.payload.quantity, 10);
+    });
   }
   return state;
 };
